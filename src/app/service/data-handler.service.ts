7b5b9536c1c98175feff0bd4f6ce6a711f448e9c
Alexander <aleksievisa@gmail.com>
2023-07-14 17:44:31 +0300

    refactor: refact. observable data-service

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,19 +2,28 @@ import { Injectable } from '@angular/core';
 import { Category } from '../model/Category';
 import { TestData } from '../data/TestData';
 import { Task } from '../model/Task';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  constructor() { }
+  tasksSubject = new BehaviorSubject<Task[]>(TestData.tasks)
+  categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories)
 
-  getCategories(): Category[] {
-    return TestData.categories;
+  constructor() {}
+
+  // getCategories(): Category[] {
+  //   return TestData.categories;
+  // }
+
+  fillTasks() {
+    this.tasksSubject.next(TestData.tasks)
   }
 
-  getTasks(): Task[] {
-    return TestData.tasks;
+  fillTasksByCategory(category: Category) {
+    const tasks = TestData.tasks.filter(task => task.category === category)
+    this.tasksSubject.next(tasks)
   }
 }
